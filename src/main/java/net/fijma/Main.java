dab64767c6749c9d89dd09fef15a4531dfd9062c
Duco Fijma <duco@fijma.net>
2025-01-11 10:50:43 +0100

    refactor reader

--- src/main/java/net/fijma/Main.java
+++ src/main/java/net/fijma/Main.java
@@ -1,91 +1,52 @@
 package net.fijma;
 
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Main {
 
-    record HomeWizardP1(double totalPowerImportKwh, double totalPowerExportKwh, double activePowerW) {}
-    record Omnik(int current, double today, double total) {}
-
-    static final Gson gson = new GsonBuilder()
-            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-            .create();
-
     public static void main(String[] args) {
-
         try {
-            final var homewizardUri = new URI("http://10.1.0.153/api/v1/data");
-            final var omnikUri = new URI("http://10.1.0.104/js/status.js");
-
-            while (!Thread.currentThread().isInterrupted()) {
-
-                try {
-                    final var p1HomeWizard = readP1HomeWizard(homewizardUri);
-                    final var omnik = readOmnik(omnikUri);
-
-                    final var activeImport = (int) p1HomeWizard.activePowerW;
-                    final var activeProduce = omnik.current;
-                    final var totalUsage = activeImport + activeProduce;
-
-                    DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-
-                    if (activeImport > 0) {
-                        System.out.println("%s: using %d (import %d, generated %d)".formatted(formatter.format(LocalDateTime.now()), totalUsage, activeImport, activeProduce));
-                    } else {
-                        System.out.println("%s: using %d (export %d, generated %d)".formatted(formatter.format(LocalDateTime.now()), totalUsage, -activeImport, activeProduce));
-
-                    }
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-
-                try {
-                    Thread.sleep(10_000);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-
-            }
-
+            loop(Reader.create());
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.exit(1);
         }
-
         System.out.println("loop terminated");
     }
 
+    private static void loop(Reader reader) {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                final var p1HomeWizard = reader.readP1HomeWizard();
+                final var omnik = reader.readOmnik();
 
-    static HomeWizardP1 readP1HomeWizard(URI uri) throws Exception {
-        final InputStreamReader reader = new InputStreamReader(uri.toURL().openStream());
-        return gson.fromJson(reader, HomeWizardP1.class);
-    }
+                final var activeImport = (int) p1HomeWizard.activePowerW();
+                final var activeProduce = omnik.current();
+                final var totalUsage = activeImport + activeProduce;
 
-    static Omnik readOmnik(URI uri) throws Exception {
-        URLConnection conn = uri.toURL().openConnection();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
-            for (String line; (line = reader.readLine()) != null; ) {
-                if (line.contains("myDeviceArray[0]=")) {
-                    final var ss = line.split(",");
-                    final int current = Integer.parseInt(ss[5]); // W
-                    final double today = Integer.parseInt(ss[6]) / 100.0; // KWh, original reading is in 1/100's of kWh
-                    final double total = Integer.parseInt(ss[7]) / 10.0; // KWh, original reading is in 1/10's of kWh
-                    return new Omnik(current, today, total);
+                DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+                if (activeImport > 0) {
+                    System.out.println("%s: using %d (import %d, generated %d)".formatted(formatter.format(LocalDateTime.now()), totalUsage, activeImport, activeProduce));
+                } else {
+                    System.out.println("%s: using %d (export %d, generated %d)".formatted(formatter.format(LocalDateTime.now()), totalUsage, -activeImport, activeProduce));
+
                 }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
             }
+
+            try {
+                Thread.sleep(10_000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
         }
-        throw new RuntimeException("Unable to find Omnik data in response");
     }
+
+
 }
---
+++ src/main/java/net/fijma/Reader.java
@@ -0,0 +1,61 @@
+package net.fijma;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
+public class Reader {
+
+    private final URL homewizardUrl;
+    private final URL omnikUrl;
+
+    private static final Gson gson = new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create();
+
+    private Reader(URL homewizardUrl, URL omnikUrl) {
+        this.homewizardUrl= homewizardUrl;
+        this.omnikUrl = omnikUrl;
+    }
+
+    public record HomeWizardP1(double totalPowerImportKwh, double totalPowerExportKwh, double activePowerW) {}
+    public record Omnik(int current, double today, double total) {}
+
+    public static Reader create() {
+        try {
+            return new Reader(
+                    new URI("http://10.1.0.153/api/v1/data").toURL(),
+                    new URI("http://10.1.0.104/js/status.js").toURL());
+        } catch (URISyntaxException | MalformedURLException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public HomeWizardP1 readP1HomeWizard() throws IOException {
+        final InputStreamReader reader = new InputStreamReader(homewizardUrl.openStream());
+        return gson.fromJson(reader, HomeWizardP1.class);
+    }
+
+    public Omnik readOmnik() throws IOException {
+        URLConnection conn = omnikUrl.openConnection();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
+            for (String line; (line = reader.readLine()) != null; ) {
+                if (line.contains("myDeviceArray[0]=")) {
+                    final var ss = line.split(",");
+                    final int current = Integer.parseInt(ss[5]); // W
+                    final double today = Integer.parseInt(ss[6]) / 100.0; // KWh, original reading is in 1/100's of kWh
+                    final double total = Integer.parseInt(ss[7]) / 10.0; // KWh, original reading is in 1/10's of kWh
+                    return new Omnik(current, today, total);
+                }
+            }
+        }
+        throw new RuntimeException("Unable to find Omnik data in response");
+    }
+
+}
